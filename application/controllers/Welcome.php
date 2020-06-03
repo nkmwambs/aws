3e39a579d59081e219fd816b13ec23c0986efafb
nkmwambs <nkmwambs@gmail.com>
2020-06-03 13:01:36 +0300

    DB test - 4

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,7 +20,7 @@ class Welcome extends CI_Controller {
 	 */
 	public function __construct(){
 		parent::__construct();
-		//$this->load->database();
+		$this->load->database();
 	}
 
 	public function index()
