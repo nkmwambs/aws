003b83425a0b19f9a2e183e62a2955ca93831759
nkmwambs <nkmwambs@gmail.com>
2020-06-04 11:13:51 +0300

    Update DB host 4

--- application/config/database.php
+++ application/config/database.php
@@ -35,8 +35,8 @@
 */
 
 // The following values will probably need to be changed.
-$db['default']['dsn'] = 'mysql:host=10.0.4.206:22;dbname=toolkit';
-$db['default']['hostname'] = "10.0.4.206:22";
+$db['default']['dsn'] = 'mysql:host=10.0.1.74;dbname=toolkit';
+$db['default']['hostname'] = "10.0.4.206";
 $db['default']['username'] = "root";
 $db['default']['password'] = "R3@l!s!ngCh!ldr3n2020";
 $db['default']['database'] = "toolkit";
