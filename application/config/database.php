258f18060a7bedd35511e784a9c0b56650b8bc14
nkmwambs <nkmwambs@gmail.com>
2020-06-04 11:15:03 +0300

    Update user

--- application/config/database.php
+++ application/config/database.php
@@ -37,8 +37,8 @@
 // The following values will probably need to be changed.
 $db['default']['dsn'] = 'mysql:host=10.0.1.74;dbname=toolkit';
 $db['default']['hostname'] = "10.0.4.206";
-$db['default']['username'] = "root";
-$db['default']['password'] = "R3@l!s!ngCh!ldr3n2020";
+$db['default']['username'] = "nkarisa";
+$db['default']['password'] = "@Compassion123";
 $db['default']['database'] = "toolkit";
 
 
