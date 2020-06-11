748d65bc21000e338f94be46599249d29088ce52
nkmwambs <nkmwambs@gmail.com>
2020-06-11 14:55:58 +0300

    Various

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -21,6 +21,7 @@ class Welcome extends CI_Controller {
 	public function __construct(){
 		parent::__construct();
 		$this->load->database();
+		$this->load->library('session');
 	}
 
 	public function index()
