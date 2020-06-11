a34656bcc9a25a45c0c92062a69eed3485d1dd71
nkmwambs <nkmwambs@gmail.com>
2020-06-11 18:58:57 +0300

    Changed implementation

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -18,11 +18,15 @@ class Welcome extends MY_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
-	public function __construct(){
-		parent::__construct();
-		//$this->load->database();
-		$this->load->library('session');
-	}
+	 public $write_db = null;
+	 public $read_db = null;
+ 
+	 function __construct(){
+		 parent::__construct();
+ 
+		 $this->read_db = $this->load->database('read_db',true);
+		 $this->write_db = $this->load->database('write_db',true);
+	 }
 
 	public function index()
 	{
