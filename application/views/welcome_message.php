e0e2066185f80663aa26d9a035fe5eb4c46d4d50
nkmwambs <nkmwambs@gmail.com>
2020-06-16 11:53:22 +0300

    Get Image from S3

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -131,6 +131,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	</div>
 	<?php 
 		var_dump($this->s3->listBuckets());
+		$obj_uri = "https://compassion-international-africa.s3-eu-west-1.amazonaws.com/image1.PNG";
+		$img = $this->s3->getObject('compassion-international-africa',$obj_uri);
 	?>
+	<img src="<?=$img;?>"/>
 </body>
 </html>
