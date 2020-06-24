416751f393391ed729e35efe3c57cb8333c76a3a
nkmwambs <nkmwambs@gmail.com>
2020-06-24 23:23:48 +0300

    Removed config from repo

--- application/config/s3_config.php
+++ application/config/s3_config.php
@@ -2,8 +2,8 @@
 
 $config = [
     's3'=>[
-        'key'=>'AKIAXRNTNANX57UFRNR6',
-        'secret'=>'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',
+        // 'key'=>'AKIAXRNTNANX57UFRNR6',
+        // 'secret'=>'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',
         'bucket'=>'compassion-international-africa'
     ]
 
