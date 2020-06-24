f4fbfa4cc10dd13864e8f1c35db8eb86813fc304
nkmwambs <nkmwambs@gmail.com>
2020-06-24 20:23:27 +0300

    Test Assume role

--- application/start.php
+++ application/start.php
@@ -5,12 +5,8 @@ require 'vendor/autoload.php';
 $config=require('config/s3_config.php');
 
 $s3Client = new S3Client([
+    'profile' => 'profile1',
     'region' => 'eu-west-1',
-    'version' => 'latest',
-    'key'    => $config['s3']['key'],
-    'secret' =>$config['s3']['secret'],
-    'credentials' => [
-        'key'    => $config['s3']['key'],
-        'secret' =>$config['s3']['secret'],
-    ],
-]);
+    'version' => '2006-03-01'
+   ]);
+   
